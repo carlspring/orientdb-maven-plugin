5f528a41eed1f1c30936e70059761a510d6224e0
baisjuan <bais.juan@gmail.com>
2016-02-22 19:03:34 -0300

    #SB-380: Create an orientdb-maven-plugin - modification of test

--- src/it/test-connection/src/test/java/org/carlspring/maven/orientdb/OrientDBTest.java
+++ src/it/test-connection/src/test/java/org/carlspring/maven/orientdb/OrientDBTest.java
@@ -1,20 +1,20 @@
 package org.carlspring.maven.orientdb;
 
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.maven.plugins.annotations.Parameter;
 import org.junit.Assert;
 import org.junit.Test;
-
-import com.tinkerpop.blueprints.impls.orient.OrientGraph;
+import com.orientechnologies.orient.client.remote.OServerAdmin;
 
 public class OrientDBTest
 {
-
-    @Test
-    public void connectionTest()
-    {
-        OrientGraph graph = new OrientGraph("plocal:C:/temp/graph/db");
-
-        Assert.assertNotNull("The graph is null!", graph);
-        graph.shutdown();
-    }
+	@Test
+	public void connectionTest() throws IOException
+	{
+		OServerAdmin serverAdmin = new OServerAdmin("remote:localhost:3015").connect("rootoo", "hello");
+		System.out.println("Succesfully connected to database");
+	}
 
 }
